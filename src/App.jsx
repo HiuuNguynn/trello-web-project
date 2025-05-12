eef37ed1e07df2016bf56736f0679ef9743ec067
Hieu Nguyen <hieunguyenby04@gmail.com>
2025-05-12 12:17:58 +0700

    update-light-dark-with-system

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,66 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
 import HomeIcon from '@mui/icons-material/Home';
 // import TabIndicator from '@mui/material/Tabs/TabIndicator';
-import {useColorScheme,} from '@mui/material/styles';
+import { useColorScheme, } from '@mui/material/styles';
+// import { useMediaQuery } from '@mui/material';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness';
+import Box from '@mui/material/Box';
+
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme();
+
+
+  const handleChange = (event) => {
+    const mode = event.target.value;
+    setMode(mode);
+  };
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light" >
+        <Box sx={{ display: 'flex', alignItems:'center', gap: 1 }}>
+        <LightModeIcon fontSize='small' /> Light
+        </Box>
+        </MenuItem>
+
+        <MenuItem value="dark" >
+        <Box sx={{ display: 'flex', alignItems:'center', gap: 1 }}>
+         <DarkModeIcon fontSize='small'/>Dark
+         </Box>
+         </MenuItem>
+
+        <MenuItem value="system" >
+        <Box sx={{ display: 'flex', alignItems:'center', gap: 1 }}>
+        <SettingsBrightnessIcon fontSize='small'/> System
+        </Box>
+        </MenuItem>
+
+      </Select>
+    </FormControl>
+  );
+}
 
 
 function ModeToggle() {
+  // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  // const prefersLightMode = useMediaQuery('(prefers-color-scheme: light)');
+  // console.log('prefersDarkMode', prefersDarkMode);
+  // console.log('prefersLightMode', prefersLightMode);
   const { mode, setMode } = useColorScheme();
   return (
     <Button
@@ -25,6 +81,8 @@ function App() {
 
   return (
     <>
+      <ModeSelect />
+      <hr />
       <ModeToggle />
       <hr />
       <div>TrungHieuDev</div>
