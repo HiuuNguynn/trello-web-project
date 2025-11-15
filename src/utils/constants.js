d29b2419ea49ff16dc8a5ceb72f1295543131bdc
Hieu Nguyen <hieunguyenby04@gmail.com>
2025-11-15 23:36:46 +0700

    fix deploy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,7 @@
 let apiRoot = ''
-if (import.meta.env.DEV || import.meta.env.MODE === 'development') {
-  apiRoot = 'http://localhost:8017'
-} else {
-  // Production API URL - thay bằng URL thực tế của bạn
-  apiRoot = import.meta.env.VITE_API_URL || 'https://trello-ykqa.onrender.com'
-}
+// if (import.meta.env.DEV || import.meta.env.MODE === 'development') {
+//   apiRoot = 'http://localhost:8017'
+// } else {
+// }
+apiRoot = 'https://trello-ykqa.onrender.com'
 export const API_ROOT = apiRoot
